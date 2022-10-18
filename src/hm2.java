863b6d49da857f239f1b093aa69d58fd3cf4aa99
HelgaEliseeva <115733274+HelgaEliseeva@users.noreply.github.com>
2022-10-19 00:36:59 +0200

    java

--- src/hm2.java
+++ src/hm2.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class hm2 {
     public static void main(String[] args) {
-        int number = 5;
-        while(number){
-            System.out.println( "Я начинаю разбираться с циклами");
+       // Найдите следующее четное число
+       Scanner scanner = new Scanner(System.in);
+       int number = scanner.nextInt();
+        if((number+1)%2==0){
+            System.out.println(number+1);
         }
+        else {
+            System.out.println(number+2);
+        }
+
+        Scanner s2 = new Scanner(System.in);
+        int wayToMary= s2.nextInt();
+        int speedTrain = s2.nextInt();
+        int travelTime = s2.nextInt();
+        int halfWay = wayToMary / 2;
+        int travelledPath =wayToMary- (speedTrain*travelTime);
+        if (travelledPath>halfWay){
+            System.out.println("ближе к Шерлоку");
+        }
+        else{
+            System.out.println("ближе к Мэри");
+        }
+
+
+
+
 
     }
 }
