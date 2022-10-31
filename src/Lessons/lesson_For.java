a9aec69f8accccc8ff0dcea5d251ee3e846ca65a
HelgaEliseeva <115733274+HelgaEliseeva@users.noreply.github.com>
2022-10-31 22:15:59 +0100

    java

---
+++ src/Lessons/lesson_For.java
@@ -0,0 +1,12 @@
+package Lessons;
+
+public class lesson_For {
+    public static void main(String[] args) {
+        //вывести четные числв от 0 до n
+        int n = 1000;
+        for (int i = 0; i<0; i+=2) {
+            System.out.println(i);
+
+        }
+    }
+}
